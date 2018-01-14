2506618f90e15e2533636a5239a7ce9487da59de
user <user@DESKTOP-C3T5D5F>
2018-01-14 11:49:39 +0800

    201801141149

--- MyJava2/src/Racing.java
+++ MyJava2/src/Racing.java
@@ -1,4 +1,6 @@
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -7,6 +9,7 @@ import javax.swing.JLabel;
 public class Racing extends JFrame {
 	private JButton go;
 	private JLabel[] lanes;
+	private Horse[] horses;
 	
 	public Racing() {
 		super("Racing Game");
@@ -21,10 +24,49 @@ public class Racing extends JFrame {
 			add(lanes[i]);
 		}
 		
+		horses = new Horse[8];
+		for (int i=0; i<horses.length; i++) {
+			horses[i] = new Horse(i);
+		}
+		
 		
 		setSize(800, 480);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		go.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				go();
+			}
+		});
+		
+		
+	}
+	
+	private void go() {
+		for (int i=0; i<horses.length; i++) {
+			horses[i].start();
+		}
+	}
+	
+	
+	private class Horse extends Thread {
+		private int num;	// 0-base
+		Horse(int n){num=n;}
+		@Override
+		public void run() {
+			for (int i=0; i<100; i++) {
+				lanes[num].setText(lanes[num].getText() + ">");
+				try {
+					Thread.sleep(50 + (int)(Math.random()*200));
+				} catch (InterruptedException e) {
+
+				}
+			}
+		}
 	}
 	
 	
