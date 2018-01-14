114a5e07857e844ead426e623c51cd3520a41f3f
user <user@DESKTOP-C3T5D5F>
2018-01-14 10:44:33 +0800

    201801141044

---
+++ MyJava2/src/Brad53.java
@@ -0,0 +1,27 @@
+
+
+public class Brad53 {
+
+	public static void main(String[] args) {
+		Brad531 obj1 = new Brad531();
+		Brad531 obj2 = new Brad531();
+		obj1.start();
+		obj2.start();
+		
+	}
+
+}
+
+class Brad531 extends Thread {
+	@Override
+	public void run() {
+		for (int i=0;i<10;i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+
+			}
+		}
+	}
+}
