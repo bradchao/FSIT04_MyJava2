8871205604670f31c83990cc4e395f1fee5ce51e
user <user@DESKTOP-C3T5D5F>
2018-01-14 15:56:27 +0800

    201801141556

--- MyJava2/src/MainBoard.java
+++ MyJava2/src/MainBoard.java
@@ -1,9 +1,13 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -12,32 +16,49 @@ import javax.swing.JPanel;
 
 public class MainBoard extends JPanel {
 	private Timer timer;
-	private int ballX, ballY;
 	private int viewW, viewH;
 	private int ballW, ballH;
-	private int dx, dy;
 	private boolean isInit;
-	private BufferedImage imgBall;
+	private BufferedImage[] imgs = new BufferedImage[5];
+	private LinkedList<Ball> balls;
 	
 	public MainBoard() {
 		setBackground(Color.GRAY);
 		timer = new Timer();
-		timer.schedule(new BallTask(), 0, 60);
-	
+		balls = new LinkedList<>();
 		
+		timer.schedule(new RefreshView(), 0, 30);
+		
+		addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mousePressed(MouseEvent event) {
+				// TODO Auto-generated method stub
+				super.mousePressed(event);
+				addNewBall(event.getX(), event.getY());
+			}
+			
+		});
+
+	}
+	
+	private void addNewBall(int eventX, int eventY) {
+		Ball ball = new Ball(eventX, eventY);
+		timer.schedule(ball, 0, 30);
+		balls.add(ball);
 	}
 	
 	private void init() {
 		isInit = true;
 		viewW = getWidth(); viewH = getHeight();
-		try {
-			imgBall = ImageIO.read(new File("imgs/ball.png"));
-			ballW = imgBall.getWidth();
-			ballH = imgBall.getHeight();
-			dx = dy = 16;
-		} catch (IOException e) {
-
+		for (int i=0; i<5; i++) {
+			try {
+				imgs[i] = ImageIO.read(new File("imgs/ball" + i +".png"));
+			} catch (IOException e) {
+	
+			}
 		}
+		ballW = ballH = 48;
 	}
 	
 	@Override
@@ -46,12 +67,30 @@ public class MainBoard extends JPanel {
 		Graphics2D g2d = (Graphics2D)g;
 		if (!isInit) init();
 		
-		g2d.drawImage(imgBall, ballX, ballY, null);
-
+		for(Ball ball : balls) {
+			g2d.drawImage(imgs[ball.imgIndex], 
+					ball.ballX, ball.ballY, null);
+		}
 
 	}
 	
-	private class BallTask extends TimerTask {
+	private class RefreshView extends TimerTask {
+		@Override
+		public void run() {
+			repaint();
+		}
+	}
+	
+	private class Ball extends TimerTask {
+		int ballX, ballY;
+		private int dx, dy;
+		int imgIndex;
+		Ball(int x, int y){
+			ballX = x; ballY = y;
+			dx = (int)(Math.random()*9) - 4;
+			dy = (int)(Math.random()*9) - 4;
+			imgIndex = (int)(Math.random()*5);
+		}
 		@Override
 		public void run() {
 			if (ballX < 0 || ballX + ballW > viewW) {
@@ -60,11 +99,11 @@ public class MainBoard extends JPanel {
 			if (ballY < 0  || ballY + ballH > viewH) {
 				dy *= -1;
 			}
-			
-			
+
 			ballX += dx;
 			ballY += dy;
-			repaint();
 		}
 	}
+
+
 }
